55a0963ba06fd8fd4feb05ca76cdca15b92546a1
auprateek <45218136+auprateek@users.noreply.github.com>
2024-06-24 06:44:06 +0000

    data type and array

---
+++ 1_Basic/array.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,4,5,6,7]
+
+// let arr2 = arr.slice(1,3)
+// console.log(arr)
+// console.log(arr2)
+let arr2 = arr.splice(1,3)
+console.log(arr)
+console.log(arr2)
---
+++ 1_Basic/datainJS.js
@@ -0,0 +1,7 @@
+const myDate = new Date();
+console.log(myDate)
+console.log("ToDateString : "+myDate.toDateString())
+console.log("TolocaledateString : "+myDate.toLocaleDateString())
+console.log("ToJson : "+myDate.toJSON())
+console.log("ToLocaleTimeString : "+myDate.toLocaleTimeString('en-IT'))
+console.log("TotimeString : "+myDate.toTimeString())
